9304ea0935463b44dcdad51a1f37ee5ef5b162e1
Leonardo Maldonado <leonardo.maldonado@namecheap.com>
2022-09-22 18:46:50 -0300

    Change on turbo json file.

--- apps/server/src/schema/types.ts
+++ apps/server/src/schema/types.ts
@@ -1,16 +1,16 @@
 /* eslint-disable */
-import type { Prisma, Admin } from "@prisma/client";
+import type { Prisma, Admin } from '@prisma/client';
 export default interface PrismaTypes {
-    Admin: {
-        Name: "Admin";
-        Shape: Admin;
-        Include: never;
-        Select: Prisma.AdminSelect;
-        OrderBy: Prisma.AdminOrderByWithRelationAndSearchRelevanceInput;
-        WhereUnique: Prisma.AdminWhereUniqueInput;
-        Where: Prisma.AdminWhereInput;
-        RelationName: never;
-        ListRelations: never;
-        Relations: {};
-    };
+  Admin: {
+    Name: 'Admin';
+    Shape: Admin;
+    Include: never;
+    Select: Prisma.AdminSelect;
+    OrderBy: Prisma.AdminOrderByWithRelationAndSearchRelevanceInput;
+    WhereUnique: Prisma.AdminWhereUniqueInput;
+    Where: Prisma.AdminWhereInput;
+    RelationName: never;
+    ListRelations: never;
+    Relations: {};
+  };
 }
